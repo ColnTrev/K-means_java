b52f66990632023985f9d3cb849bcaa32664a1c3
Collin Gordon <colntrev@gmail.com>
2017-02-13 11:03:26 -0800

    update

--- Centroid.java
+++ Centroid.java
@@ -1,25 +1,92 @@
+
 package MASS.KMeans_java;
-import edu.uw.bothell.css.dsl.MASS.Place;
+import edu.uw.bothell.css.dsl.MASS.*;
 
 public class Centroid extends Place {
     public static final int init_    = 0;
-    public static final int mean_    = 1;
-    public static final int update_  = 2;
-    public static final int sig_     = 3;
-    public static final int findv_   = 4;
-    public static final int getNext_ = 5;
-    public static final int rept_    = 6;
+    public static final int update_  = 1;
+    public static final int sig_     = 2;
+    public static final int findv_   = 3;
+    public static final int getNext_ = 4;
+    public static final int rept_    = 5;
+
+    private int mean;
+    private int csize;
+    private int[] nextMove;
+    private int arrX, arrY, localX, localY; // array dimensions and local dimensions
+
+    public Centroid(){
+        super();
+        nextMove = new int[2];
+        Vector<int[]> neighbors = new Vector<>();
+        neighbors.add(new int[]{0,-1});
+        neighbors.add(new int[]{1,0});
+        neighbors.add(new int[]{0,1});
+        neighbors.add(new int[]{-1,0});
+        setNeighbors(neighbors);
+    }
+    
+    public Centroid(Object obj){
+        super();
+        nextMove = new int[2];
+        Vector<int[]> neighbors = new Vector<>();
+        neighbors.add(new int[]{0,-1});
+        neighbors.add(new int[]{1,0});
+        neighbors.add(new int[]{0,1});
+        neighbors.add(new int[]{-1,0});
+        setNeighbors(neighbors);
+    }
 
     public static Object callMethod(int functionId, Object args){
         switch(functionId){
             case init_:    return init(args);
-            case mean_:    return getMean(args);
             case update_:  return update();
             case sig_:     return signal();
             case findv_:   return findValid();
             case getNext_: return sendNextMove();
-            case rept_:    return report();
         }
         return null;
     }
+
+    // initializes essential data for the Centroid
+    private Object init(){
+        arrX = getSize()[0];
+        arrY = getSize()[1];       // places array size
+        localX = getIndex()[0]; localY = getIndex()[1]; // my coordinates
+        mean = Math.random() % 20 + 1;
+        csize = 0;
+        return null;
+    }
+
+    //returns mean to point
+    public int getMean(){
+        return mean;
+    }
+
+    // checks inmessages for a valid place to move the unlocked points
+    private Object findValid(){
+        return null;
+    }
+
+    //sends next move to unlocked points
+    public int[] sendNextMove(){
+        return nextMove;
+    }
+
+    // updates mean of centroids
+    private Object update(){
+        int sum = 0;
+        int total = getAgents().size();
+        
+        // loop logic to sum up weights of agents and compute new mean
+        
+        /* Set<Point> points = getAgents();
+           Iterator iter = points.iterator();
+           while(iter.hasNext()){
+               sum += iter.next().getWeight();
+           }
+           mean = sum / total; */
+        return null; 
+    }
+
 }
--- Point.java
+++ Point.java
@@ -1,6 +1,5 @@
 package MASS.Kmeans_java;
-import edu.uw.bothell.css.dsl.MASS.Place;
-import edu.uw.bothell.css.dsl.MASS.Agent;
+import edu.uw.bothell.css.dsl.MASS.*;
 
 public class Point extends Agent{
         public static final int set_    = 0;
@@ -9,13 +8,58 @@ public class Point extends Agent{
         public static final int dist_   = 3;
         public static final int rept_   = 4;
 
+        private Vector<int[]> neighbors = new Vector<>();
+        private int weight;
+        private boolean lock;
+        private int[] movement;
+        
+        public Point(){
+            super();
+        }
+
+        public Point(Object obj){
+            super();
+            movement = new int[2];
+            neighbors.add(new int[]{0, -1});
+            neighbors.add(new int[]{1, 0});
+            neighbors.add(new int[]{0, 1});
+            neighbors.add(new int[]{-1, 0});
+            setWeight();
+        }
         public static Object callMethod(int functionId, Object args){
             switch(functionId){
                 case set_:      return setWeight();
                 case move_:     return move();
                 case weight_:   return getWeight(args);
                 case dist_:     return dist();
-                case rept_:     return report();
+            }
+            return null;
+        }
+
+        private Object setWeight(){
+            weight = Math.random() % 20 + 1;
+            return null;
+        }
+
+        public int getWeight(){
+            return weight;
+        }
+
+        private Object distance(){
+            int m = getPlace().getMean();
+            int dist = Math.abs(weight - m);
+            if(dist <= m){
+                lock = true;
+            } else {
+                lock = false;
+            }
+            return null;
+        }
+
+        private Object move(){
+            if(!lock){
+                movement = getPlace().sendNextMove();
+                migrate(movement[0], movement[1]);
             }
             return null;
         }
